be1736e2a516b9403f49b9d92b5ab06da85a985a
Cesar <cesarvillalobosolmos.01@gmail.com>
2024-03-23 20:32:35 -0600

    :art: Add background and fruits design

---
+++ src/components/fruits.tsx
@@ -0,0 +1,12 @@
+export default function Fruits() {
+  return (
+    <>
+      <span className='absolute top-2 left-40'>🍉</span><span className='absolute top-48 left-36'>🍊</span><span className='absolute bottom-72 right-52'>🍊</span>
+      <span className='absolute bottom-20 left-40'>🍊</span><span className='absolute top-80 right-32'>🍋</span><span className='absolute top-96 left-48'>🍌</span>
+      <span className='absolute top-24 left-12'>🍍</span><span className='absolute top-64 left-24'>🥭</span><span className='absolute top-60 right-36'>🍎</span>
+      <span className='absolute top-60 right-14'>🍏</span><span className='absolute bottom-8 right-64'>🍐</span><span className='absolute top-8 left-5'>🍑</span>
+      <span className='absolute bottom-12 left-7'>🍒</span><span className='absolute bottom-32 right-32'>🍓</span><span className='absolute top-96 right-5'>🥝</span>
+      <span className='absolute top-96 left-5'>🍅</span><span className='absolute top-28 right-56'>🥥</span><span className='absolute top-2 right-5'>🫐</span>
+    </>
+  )
+}
